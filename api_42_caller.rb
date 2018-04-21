8983f8e66ecbfa55674a3d3a69fdffaab28c2d5f
Clurquiz <clementine.urquizar@gmail.com>
2018-04-21 02:53:52 +0200

    upd : instance method in api42 caller

--- api_42_caller.rb
+++ api_42_caller.rb
@@ -6,36 +6,37 @@ class API42Caller
   SECRET   = ENV['SECRET_42']
   ENDPOINT = 'https://api.intra.42.fr'
 
-  class << self
+  attr_reader :token
 
-    def request_locations_for user, range
-      get_request locations_url_for(user), range_param(range)
-    end
-
-    def get_request url, params = {}
-      token = get_access_token
-      response = token.get url, params: params
-      response.parsed
-    end
-
-    private
-
-    def get_access_token
-      client = oauth2_connection
-      client.client_credentials.get_token
-    end
-
-    def oauth2_connection
-      OAuth2::Client.new(UID, SECRET, site: ENDPOINT)
-    end
-
-    def locations_url_for user
-      "/v2/users/#{ user }/locations"
-    end
-
-    def range_param range
-      { range: { begin_at: range } }
-    end
-
+  def initialize
+    @token = get_access_token
   end
+
+  def request_locations_for user, range
+    get_request locations_url_for(user), range_param(range)
+  end
+
+  def get_request url, params = {}
+    response = token.get url, params: params
+    response.parsed
+  end
+
+  private
+
+  def locations_url_for user
+    "/v2/users/#{ user }/locations"
+  end
+
+  def range_param range
+    { range: { begin_at: range } }
+  end
+
+  def get_access_token
+    oauth2_connection.client_credentials.get_token
+  end
+
+  def oauth2_connection
+    OAuth2::Client.new(UID, SECRET, site: ENDPOINT)
+  end
+
 end
--- main_script.rb
+++ main_script.rb
@@ -1,4 +1,5 @@
 require_relative 'api_42_caller'
 require 'pry'
 
-puts API42Caller.request_locations_for('curquiza', '2017-01-01,2018-04-10')
+# puts API42Caller.request_locations_for('curquiza', '2017-01-01,2018-04-10')
+puts API42Caller.new.request_locations_for('curquiza', '2017-01-01,2018-04-10')
