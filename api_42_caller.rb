8fee6ea7d83823f976dd860df871d858d1a2c70d
Clurquiz <clementine.urquizar@gmail.com>
2018-04-21 03:06:52 +0200

    add : new class oauth2caller

--- api_42_caller.rb
+++ api_42_caller.rb
@@ -1,24 +1,13 @@
-require 'oauth2'
+require_relative 'oauth2_caller'
 
-class API42Caller
+class API42Caller < OAuth2Caller
 
   UID      = ENV['UID_42']
   SECRET   = ENV['SECRET_42']
   ENDPOINT = 'https://api.intra.42.fr'
 
-  attr_reader :token
-
-  def initialize
-    @token = get_access_token
-  end
-
   def request_locations_for user, range
     get_request locations_url_for(user), range_param(range)
-  end
-
-  def get_request url, params = {}
-    response = token.get url, params: params
-    response.parsed
   end
 
   private
@@ -29,14 +18,6 @@ class API42Caller
 
   def range_param range
     { range: { begin_at: range } }
-  end
-
-  def get_access_token
-    oauth2_connection.client_credentials.get_token
-  end
-
-  def oauth2_connection
-    OAuth2::Client.new(UID, SECRET, site: ENDPOINT)
   end
 
 end
