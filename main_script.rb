d10dcb77f32ef87a17b76d11c3d4201acc16dabc
Clurquiz <clementine.urquizar@gmail.com>
2018-04-23 23:42:22 +0200

    upd : main

--- main_script.rb
+++ main_script.rb
@@ -1,4 +1,9 @@
 require_relative 'api_42_caller'
+require_relative 'header_link'
 require 'pry'
 
-puts API42Caller.new.request_locations_for('curquiza', '2017-01-01,2018-04-10')
+response = API42Caller.new.request_locations_for('curquiza', '2017-01-01,2018-04-10')
+puts response
+puts '\n'
+response_next = HeaderLink.next response
+puts response_next
