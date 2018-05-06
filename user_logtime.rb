ec532759be87b3fc0cd6d2f6e555b2a74ea8cc85
Clurquiz <clementine.urquizar@gmail.com>
2018-05-06 21:40:43 +0200

    upd refacto of quarter_ranges

--- user_logtime.rb
+++ user_logtime.rb
@@ -2,10 +2,11 @@ require_relative 'logtime_calculator'
 
 class UserLogtime
 
-  attr_reader :user, :range
+  attr_reader :user, :year, :range
 
   def initialize user, params = {}
     @user  = user
+    @year  = params[:year] || Time.now.year
     @range = determine_range params
     puts "RANGE = #{ range }\n-----"
   end
@@ -17,19 +18,17 @@ class UserLogtime
   private
 
   def determine_range params
-    year = params[:year] || Time.now.year
-    case params[:quarter]
-    when 1
-      "#{ year }-01-01,#{ year }-03-31"
-    when 2
-      "#{ year }-04-01,#{ year }-06-30"
-    when 3
-      "#{ year }-07-01,#{ year }-09-30"
-    when 4
-      "#{ year }-10-01,#{ year }-12-31"
-    else
-      "#{ year }-01-01,#{ year }-12-31"
-    end
+    quarter_ranges["q#{ params[:quarter] }"] || quarter_ranges[:default]
+  end
+
+  def quarter_ranges
+    @quarter_ranges ||= {
+      q1:      "#{ year }-01-01,#{ year }-03-31",
+      q2:      "#{ year }-04-01,#{ year }-06-30",
+      q3:      "#{ year }-07-01,#{ year }-09-30",
+      q4:      "#{ year }-10-01,#{ year }-12-31",
+      default: "#{ year }-01-01,#{ year }-12-31"
+    }
   end
 
 end
