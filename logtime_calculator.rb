be26742be02ea235c3b92c905cff0c70430ce8f1
Clurquiz <clementine.urquizar@gmail.com>
2018-05-06 14:59:43 +0200

    add logtime calculator class

---
+++ logtime_calculator.rb
@@ -0,0 +1,34 @@
+require_relative 'api_42_caller'
+require_relative 'header_link'
+
+class LogtimeCalculator
+
+  class << self
+
+    def total_logtime_hours user, range
+      response = API42Caller.new.request_locations_for(user, range)
+      total = logtime_hours_in(response[:content])
+      while (response = HeaderLink.next response[:links])
+        total += logtime_hours_in response[:content]
+      end
+      total
+    end
+
+    private
+
+    def logtime_hours_in content_hash
+      content_hash.sum do |line|
+        hours_btw_two_dates(line['end_at'], line['begin_at'])
+      end
+    end
+
+    def hours_btw_two_dates str_1, str_2
+      (str_to_time(str_1) - str_to_time(str_2)) / 3600
+    end
+
+    def str_to_time str
+      Time.parse str
+    end
+
+  end
+end
--- main_script.rb
+++ main_script.rb
@@ -1,35 +1,4 @@
-require_relative 'api_42_caller'
-require_relative 'header_link'
 require 'pry'
+require_relative 'logtime_calculator'
 
-def total_logtime_hours
-  response = API42Caller.new.request_locations_for('curquiza', '2018-04-01,2018-04-10')
-  total = logtime_hours_in(response[:content])
-  while (response = HeaderLink.next response[:links])
-    total += logtime_hours_in response[:content]
-  end
-  total
-end
-
-def logtime_hours_in content_hash
-  content_hash.sum do |line|
-    hours_btw_two_dates(line['end_at'], line['begin_at'])
-  end
-end
-
-def hours_btw_two_dates str_1, str_2
-  (str_to_time(str_1) - str_to_time(str_2)) / 3600
-end
-
-def str_to_time str
-  Time.parse str
-end
-
-# response = API42Caller.new.request_locations_for('curquiza', '2017-01-01,2018-04-10')
-# puts response[:content]
-# puts logtime_hours_in response[:content]
-# puts "\n"
-# response_next = HeaderLink.next response[:links]
-# puts response_next[:content]
-
-puts total_logtime_hours
+puts LogtimeCalculator.total_logtime_hours 'curquiza', '2018-04-01,2018-06-10'
