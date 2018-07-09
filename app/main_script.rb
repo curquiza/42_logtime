96cd8cb264cc3ab1050ed841f4a0f676daef0e2e
Clurquiz <clementine.urquizar@gmail.com>
2018-07-10 00:34:08 +0200

    upd main script

--- app/main_script.rb
+++ app/main_script.rb
@@ -1,7 +1,14 @@
 require 'pry'
 require_relative 'user_logtime'
 
-user_logtime = UserLogtime.new('curquiza', {year: 2018, quarter: 1})
+# user_logtime = UserLogtime.new('curquiza', {year: 2018, quarter: 1})
 # user_logtime = UserLogtime.new('curquiza', {year: 2017})
-puts user_logtime.compute
+# puts user_logtime.compute
 # puts user_logtime.range
+
+# require_relative 'message_parser'
+#
+# a = MessageParser.new("!logtime curquiza 2018 q1")
+# puts "login = #{a.login}"
+# puts "quarter id = #{a.quarter}"
+# puts "year = #{a.year}"
