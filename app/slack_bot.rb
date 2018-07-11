9eb6dd746cb10aba6447851f6627a2f80bc8fdde
Clurquiz <clementine.urquizar@gmail.com>
2018-07-11 09:43:06 +0200

    wip

--- app/slack_bot.rb
+++ app/slack_bot.rb
@@ -13,17 +13,43 @@ Slack.configure do |config|
   config.token = ENV['SLACK_API_TOKEN']
 end
 
-realtime_client = Slack::RealTime::Client.new
+# Slack::RealTime::Client.configure do |config|
+#   config.start_method = :rtm_start
+# end
 
-realtime_client.on :message do |data|
-  p "message : #{data.text}"
-  request = MessageParser.new(data.text)
-  # puts "login = #{request.login}"
-  # puts "quarter id = #{request.quarter}"
-  # puts "year = #{request.year}"
-  user_logtime = UserLogtime.new(request.login, {year: request.year, quarter: request.quarter})
-  puts "range = #{ user_logtime.range }\n"
-  rslt = user_logtime.compute
-  puts "rslt = #{ rslt }\n-----"
+# client = Slack::Web::Client.new
+# client.auth_test
+#
+# realtime_client = Slack::RealTime::Client.new
+#
+# realtime_client.on :message do |data|
+#   p "message : #{data.text}"
+#   # request = MessageParser.new(data.text)
+#   # puts "login = #{request.login}"
+#   # puts "quarter id = #{request.quarter}"
+#   # puts "year = #{request.year}"
+#   # user_logtime = UserLogtime.new(request.login, {year: request.year, quarter: request.quarter})
+#   # puts "range = #{ user_logtime.range }\n"
+#   # rslt = user_logtime.compute
+#   # puts "rslt = #{ rslt }\n-----"
+#
+#   client.realtime_client.chat_postMessage(channel: data.channel, text: 'Ciao les nazes', as_user: true)
+#   # client.reactions_add name: 'poop', timestamp: data.ts, channel: data.channel
+# end
+# realtime_client.start!
+
+client = Slack::RealTime::Client.new
+
+client.on :hello do
+  puts "Successfully connected - login: '#{client.self.name}' - team : '#{client.team.name}' - https://#{client.team.domain}.slack.com."
 end
-realtime_client.start!
+
+client.on :message do |data|
+  case data.text
+  when 'bot hi' then
+    client.message channel: data.channel, text: "Hi <@#{data.user}>!"
+  when /^bot/ then
+    client.message channel: data.channel, text: "Sorry <@#{data.user}>, what?"
+  end
+end
+client.start!
